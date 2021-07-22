982f3f325df0eea2a366c4c6eda3869193c5089a
Beatriz Martinez <trism@gmail.com>
2021-07-22 15:46:54 -0500

    Fixes to try-catch statement

---
+++ cfeScraping/index.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+const URL = 'https://msc.cfe.mx/Aplicaciones/NCFE/Concursos/';
+OpenBrowser = async () => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.goto(URL);
+        await page.waitForSelector('div.gti-app');
+        console.log('Successful');
+
+        await page.waitForTimeout(2000);
+        await page.screenshot({ path: 'pictures/Cfe.png' });
+
+
+        await browser.close();
+    } catch (error) {
+        console.error('An error have been occurred: ' + error);
+    }
+};
+module.exports = {
+    "OpenBrowser": OpenBrowser
+}
+
+
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-try {
-    const OpenBrowser = require('./cfeScraping/index.js');
+const goCFE = require('./cfeScraping/index.js');
 
-    'use strict';
-    // we're gonna use strict  mode in all
+'use strict';
+// we're gonna use strict  mode in all
 
-    console.log('Hello Message');
-    OpenBrowser.OpenBrowser();
-
-} catch (err) {
-    console.error('An error have been occurred:', err);
-}
+console.log('Hello Message');
+goCFE.OpenBrowser();
