fb782d1f0c5b51b9438dcec45a22df140df3580b
Beatriz Martinez <trism@gmail.com>
2021-08-05 15:14:50 -0500

    Corrections in the promise & class (incomplete)

--- cfeScraping/cScrapPage.js
+++ cfeScraping/cScrapPage.js
@@ -1,29 +1,51 @@
 /* eslint-disable require-jsdoc */
 'use strict';
 
-// const puppeteer = require('puppeteer');
-
-class ScrapPage {
-  constructor(browser) {
-    this.browser = browser;
-    this.openNewPage();
-  }
-  async openNewPage() {
-    try {
-      this.page = await this.browser.newPage();
-    } catch (err) {
-      console.error('Error: ', err);
-    }
-  }
-  async navigatePage(URLPage) {
-    try {
-      this.page = await this.page.goto(URLPage);
-    } catch (err) {
-      console.error('Error: ', err);
-    }
-  }
+function ScrapPage(browser) {
+  this.browser = browser;
+  this.page = browser.page;
 }
 
-module.exports = {
-  ScrapPage: ScrapPage,
+// eslint-disable-next-line prettier/prettier
+ScrapPage.prototype.openNewPage = async function(URLPage) {
+  try {
+    this.page = await this.browser.newPage();
+    console.log('Opening a new tab...');
+    await this.page.goto(URLPage);
+    return console.log(`${URLPage} has been opened successfully`);
+  } catch (err) {
+    return console.error('Error: ', err);
+  }
 };
+
+// eslint-disable-next-line prettier/prettier
+// ScrapPage.prototype.navigatePage = async function() {
+//   try {
+//     await this.page.goto(URLPage);
+//   } catch (err) {
+//     return console.error('Error4: ', err);
+//   }
+// };
+
+// class ScrapPage {
+//   constructor(browser) {
+//     this.browser = browser;
+//     this.openNewPage();
+//   }
+//   async openNewPage() {
+//     try {
+//       this.page = await browser.newPage();
+//     } catch (err) {
+//       return console.error('Error3: ', err);
+//     }
+//   }
+//   async navigatePage(URLPage) {
+//     try {
+//       this.page = await this.page.goto(URLPage);
+//     } catch (err) {
+//       return console.error('Error: ', err);
+//     }
+//   }
+// }
+
+module.exports = ScrapPage;
--- index.js
+++ index.js
@@ -1,31 +1,76 @@
+/* eslint-disable require-jsdoc */
 /* eslint-disable indent */
 'use strict';
 // we're gonna use strict  mode in all
 
-// const goCFE = require('./cfeScraping/index.js');
-
-// eslint-disable-next-line object-curly-spacing
 const puppeteer = require('puppeteer');
 const URLPage = 'https://msc.cfe.mx/Aplicaciones/NCFE/Concursos/';
 const ScrapPage = require('./cfeScraping/cScrapPage');
 
 // eslint-disable-next-line space-before-function-paren
-new Promise(function () {
-  const browser = puppeteer
-    .launch()
-    .then(() => {
-      const myPage = new ScrapPage(browser);
-      myPage.navigatePage(URLPage);
-    })
-    .catch((err) => {
-      console.error('Error: ', err);
-    });
-});
+// new Promise(function () {
+//   const browser = puppeteer
+//     .launch()
+//     .then(() => {
+//       const myPage = new ScrapPage(browser);
+//       myPage.openNewPage();
+//       myPage.navigatePage(URLPage);
+//     })
+//     .catch((err) => {
+//       console.error('Error: ', err);
+//     });
+// });
 
-// const data = {
-//   idInput: 'descProc',
-//   textInput: 'IoT',
-//   button: 'buscar',
-// };
+async function newBrowser() {
+  const browser = await puppeteer.launch();
+  console.log('Opening a new browser...');
+  return browser;
+}
 
-// myPage.openBrsowser();
+const myPromise = () => {
+  return new Promise((resolve, reject) => {
+    const browser = newBrowser();
+    if (1 + 1 == 2) {
+      resolve(browser);
+    } else {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('Cannot open a browser');
+    }
+  });
+};
+
+myPromise()
+  .then((res) => {
+    const browser = res;
+    const myPage = new ScrapPage(browser);
+    myPage.openNewPage(URLPage);
+    // })
+    //   .then(() => {
+    // myPage.navigatePage(URLPage);
+    return;
+  })
+  .catch((err) => {
+    return console.error('Error: ', err);
+  });
+
+// let browser;
+
+// async function newBrowser() {
+//   browser = await puppeteer.launch();
+// }
+
+// new Promise(() => {
+//   browser = newBrowser();
+//   const myPage = new ScrapPage(browser);
+//   console.log('Browser 2', myPage);
+// })
+//   .then((res) => {
+//     myPage.openNewPage();
+//     console.log('Open page');
+
+//     myPage.navigatePage(URLPage);
+//     console.log('res ', res);
+//   })
+//   .catch((err) => {
+//     console.error('Error1: ', err);
+//   });
