0e2ecf32724f7d00f9dfd2c3fc5c3568973e71f1
Beatriz Martinez <trism@gmail.com>
2021-08-06 15:40:23 -0500

    Add an async function in index

--- cfeScraping/cScrapPage.js
+++ cfeScraping/cScrapPage.js
@@ -3,49 +3,18 @@
 
 function ScrapPage(browser) {
   this.browser = browser;
-  this.page = browser.page;
 }
 
 // eslint-disable-next-line prettier/prettier
 ScrapPage.prototype.openNewPage = async function(URLPage) {
   try {
-    this.page = await this.browser.newPage();
+    const page = await this.browser.newPage();
     console.log('Opening a new tab...');
-    await this.page.goto(URLPage);
+    await page.goto(URLPage);
     return console.log(`${URLPage} has been opened successfully`);
   } catch (err) {
     return console.error('Error: ', err);
   }
 };
-
-// eslint-disable-next-line prettier/prettier
-// ScrapPage.prototype.navigatePage = async function() {
-//   try {
-//     await this.page.goto(URLPage);
-//   } catch (err) {
-//     return console.error('Error4: ', err);
-//   }
-// };
-
-// class ScrapPage {
-//   constructor(browser) {
-//     this.browser = browser;
-//     this.openNewPage();
-//   }
-//   async openNewPage() {
-//     try {
-//       this.page = await browser.newPage();
-//     } catch (err) {
-//       return console.error('Error3: ', err);
-//     }
-//   }
-//   async navigatePage(URLPage) {
-//     try {
-//       this.page = await this.page.goto(URLPage);
-//     } catch (err) {
-//       return console.error('Error: ', err);
-//     }
-//   }
-// }
 
 module.exports = ScrapPage;
--- index.js
+++ index.js
@@ -7,70 +7,18 @@ const puppeteer = require('puppeteer');
 const URLPage = 'https://msc.cfe.mx/Aplicaciones/NCFE/Concursos/';
 const ScrapPage = require('./cfeScraping/cScrapPage');
 
-// eslint-disable-next-line space-before-function-paren
-// new Promise(function () {
-//   const browser = puppeteer
-//     .launch()
-//     .then(() => {
-//       const myPage = new ScrapPage(browser);
-//       myPage.openNewPage();
-//       myPage.navigatePage(URLPage);
-//     })
-//     .catch((err) => {
-//       console.error('Error: ', err);
-//     });
-// });
-
 async function newBrowser() {
   const browser = await puppeteer.launch();
   console.log('Opening a new browser...');
   return browser;
 }
 
-const myPromise = () => {
-  return new Promise((resolve, reject) => {
-    const browser = newBrowser();
-    if (1 + 1 == 2) {
-      resolve(browser);
-    } else {
-      // eslint-disable-next-line prefer-promise-reject-errors
-      reject('Cannot open a browser');
-    }
-  });
-};
-
-myPromise()
-  .then((res) => {
-    const browser = res;
+(async () => {
+  try {
+    const browser = await newBrowser();
     const myPage = new ScrapPage(browser);
     myPage.openNewPage(URLPage);
-    // })
-    //   .then(() => {
-    // myPage.navigatePage(URLPage);
-    return;
-  })
-  .catch((err) => {
-    return console.error('Error: ', err);
-  });
-
-// let browser;
-
-// async function newBrowser() {
-//   browser = await puppeteer.launch();
-// }
-
-// new Promise(() => {
-//   browser = newBrowser();
-//   const myPage = new ScrapPage(browser);
-//   console.log('Browser 2', myPage);
-// })
-//   .then((res) => {
-//     myPage.openNewPage();
-//     console.log('Open page');
-
-//     myPage.navigatePage(URLPage);
-//     console.log('res ', res);
-//   })
-//   .catch((err) => {
-//     console.error('Error1: ', err);
-//   });
+  } catch (err) {
+    console.error('Error: ', err);
+  }
+})();
