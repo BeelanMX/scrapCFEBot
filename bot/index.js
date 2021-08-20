ba55baeb2801f88c0276089d4ef6c9ba44c84c35
Lucy Guzmán <analucygdl@gmail.com>
2021-08-20 18:02:13 -0500

    Bot ready console message

--- bot/index.js
+++ bot/index.js
@@ -1,3 +1,10 @@
 console.log('Hello');
 
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+client.on('ready', readyDiscord);
+
+function readyDiscord() {
+    console.log('Aloh');
+}
