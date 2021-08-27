911bc790f2c580b3e4224f6f89f7a0abdc257493
Lucy Guzmán <analucygdl@gmail.com>
2021-08-26 20:25:30 -0500

    Command changes (On work)

--- bot/index.js
+++ bot/index.js
@@ -6,15 +6,33 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 client.on('ready', readyDiscord);
 
 function readyDiscord() {
-    console.log('Aloh');
+    console.log('Ready');
 }
 
-client.on('message', gotMessage);
-function gotMessage(msg) {
-    console.log(msg.content);
-    if (msg.content === '!scrapbot') {
-        msg.reply('Hello');
+//Assign the value "!" to the constant prefix, which you will use as the bot prefix.
+const prefix = "!";
+
+//Check if the content of the message that the bot is processing starts with
+//the prefix you set and if it doesn't stop processing
+client.on('message', function (message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    //Convert the rest of the message to a command name and any arguments that
+    //may exist in the message.
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "scrapbot") {
+        message.reply('Hello');
     }
-}
+    else if (command === "iot") {
+        message.reply('IoT');
+    }
+    else if (command === "electronica") {
+        message.reply('Electronica');
+    }
+});
 
 client.login('');
