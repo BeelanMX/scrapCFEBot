35d4c0bd842a1c8aafbf798dd79a33d957ca8e69
Lucy Guzmán <analucygdl@gmail.com>
2021-08-31 15:14:59 -0500

    Test w/ sintaxis changes

--- bot/index.js
+++ bot/index.js
@@ -2,6 +2,7 @@ console.log('Hello');
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const config = require("./config.json");
 
 client.on('ready', readyDiscord);
 
@@ -10,7 +11,7 @@ function readyDiscord() {
 }
 
 //Assign the value "!" to the constant prefix, which you will use as the bot prefix.
-const prefix = "!";
+const prefix = config.prefix;
 
 //Check if the content of the message that the bot is processing starts with
 //the prefix you set and if it doesn't stop processing
@@ -20,20 +21,19 @@ client.on('message', function (message) {
 
     //Convert the rest of the message to a command name and any arguments that
     //may exist in the message.
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
+    let texto = args.join(" ").toLowerCase();
 
     if (command === "scrapbot") {
         message.reply('Hello');
     }
-    else if (command === "cfe") {
-        if (args === "iot") {
-            message.reply('IoT');
+    else
+        if (command === "cfe") {
+            if (!texto) return message.channel.send(`Especifique`);
+            if (texto === "iot") message.channel.send('IoT');
+            if (texto === "electronica") message.channel.send('Electronica');
         }
-        else if (args === "electronica")
-            message.reply('Electronica');
-    }
 });
 
-client.login('');
+client.login(config.token);
