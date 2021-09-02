e54826779d4d2bd88d808d1e7281f66cff4d0e49
Lucy Guzmán <analucygdl@gmail.com>
2021-09-01 19:42:53 -0500

    Separate command !cfe and testing functionality

---
+++ bot/commands/cfe.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'cfe',
+	description: 'Get searching parameters from the user.',
+	execute(msg) {
+		msg.channel.send('CFE.');
+	},
+};
--- bot/index.js
+++ bot/index.js
@@ -1,17 +1,29 @@
 console.log('Hello');
 
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require("./config.json");
+
+//Assign the value "!" to the constant prefix, which you will use as the bot prefix.
+const prefix = config.prefix;
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
 
 client.on('ready', readyDiscord);
 
 function readyDiscord() {
     console.log('Ready');
 }
-
-//Assign the value "!" to the constant prefix, which you will use as the bot prefix.
-const prefix = config.prefix;
 
 //Check if the content of the message that the bot is processing starts with
 //the prefix you set and if it doesn't stop processing
@@ -21,14 +33,20 @@ client.on('message', function (message) {
 
     //Convert the rest of the message to a command name and any arguments that
     //may exist in the message.
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    let texto = args.join(" ").toLowerCase();
 
-    if (command === "cfe") {
-        if (!texto) return message.channel.send('The command needs a searching parameter');
-        message.channel.send(texto);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+    const command = client.commands.get(commandName);
+    try {
+
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('Error trying to execute that command.');
     }
 });
+
 
 client.login(config.token);
