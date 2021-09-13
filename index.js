580470249798d7e3386dbb439d1ef919bfbeeb92
Lucy Guzmán <analucygdl@gmail.com>
2021-09-13 14:16:41 -0500

    Fixing problem with cooldowns

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const fs = require('fs');
-const token = process.env.TOKEN;
+const Discord = require('discord.js')
 const cooldowns = new Discord.Collection();
 const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -34,9 +34,9 @@ client.on('message', function (message) {
     //Convert the rest of the message to a command name and any arguments that
     //may exist in the message.
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
-    let text = args.join(" ");
+    //let text = args.join(" ");
 
 
     if (!client.commands.has(commandName)) return;
@@ -66,4 +66,4 @@ client.on('message', function (message) {
 });
 
 
-client.login(token);
+client.login(process.env.TOKEN);
