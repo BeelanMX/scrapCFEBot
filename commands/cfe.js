934ae75989a22d0825aa34fad925bcb0fef37321
Lucy Guzmán <analucygdl@gmail.com>
2021-09-14 18:04:09 -0500

    Fixing cooldown

--- commands/cfe.js
+++ commands/cfe.js
@@ -1,9 +1,25 @@
+const talkedRecently = new Set();
 module.exports = {
-	name: 'cfe',
-	description: 'Get searching parameters from the user.',
-	cooldown: 3,
-	execute(message, args) {
-		if (!message) return message.channel.send('The command needs a searching parameter.');
-		message.channel.send(message);
-	},
+  name: 'cfe',
+  description: 'Get searching parameters from the user.',
+  cooldown: 3,
+  execute(message, args) {
+    if (!message) return message.channel.send('The command needs a searching parameter.');
+    message.channel.send(message);
+
+    if (talkedRecently.has(message.author.id)) {
+      message.channel.send("Wait  minute before getting typing this again. - ");
+    } else {
+
+      // the user can type the command ... your command code goes here :)
+
+      // Adds the user to the set so that they can't talk for a minute
+      talkedRecently.add(message.author.id);
+      setTimeout(() => {
+        // Removes the user from the set after a minute
+        talkedRecently.delete(message.author.id);
+      }, 60000);
+    }
+
+  },
 };
--- index.js
+++ index.js
@@ -13,56 +13,40 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    // Set a new item in the Collection
-    // With the key as the command name and the value as the exported module
-    client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.name, command);
 }
 
 client.on('ready', readyDiscord);
 
 function readyDiscord() {
-    console.log('Ready');
+  console.log('Ready');
 }
 
 //Check if the content of the message that the bot is processing starts with
 //the prefix you set and if it doesn't stop processing
 client.on('message', function (message) {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
-    //Convert the rest of the message to a command name and any arguments that
-    //may exist in the message.
+  //Convert the rest of the message to a command name and any arguments that
+  //may exist in the message.
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-    //let text = args.join(" ");
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+  //let text = args.join(" ");
 
 
-    if (!client.commands.has(commandName)) return;
-    const command = client.commands.get(commandName);
-    try {
-        if (!cooldowns.has(command.name)) {
-            cooldowns.set(command.name, new Discord.Collection());
-        }
-
-        const now = Date.now();
-        const timestamps = cooldowns.get(command.name);
-        const cooldownAmount = (command.cooldown || 3) * 1000;
-
-        if (timestamps.has(message.author.id)) {
-            const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-            if (now < expirationTime) {
-                const timeLeft = (expirationTime - now) / 1000;
-                return message.reply(`Hey, wait ${timeLeft.toFixed(1)} more second(s) the \`${command.name}\` command. NOT SPAM`);
-            }
-        }
-        command.execute(message, args);
-    } catch (error) {
-        console.error(error);
-        message.reply('Error trying to execute that command.');
-    }
+  if (!client.commands.has(commandName)) return;
+  const command = client.commands.get(commandName);
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('Error trying to execute that command.');
+  }
 });
 
 
