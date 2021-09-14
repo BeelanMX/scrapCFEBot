686ae8066cf730740e655c35e4a850d4dadfc78a
Lucy Guzmán <analucygdl@gmail.com>
2021-09-14 18:33:33 -0500

    Cooldown working

--- commands/cfe.js
+++ commands/cfe.js
@@ -1,25 +1,9 @@
-const talkedRecently = new Set();
 module.exports = {
   name: 'cfe',
   description: 'Get searching parameters from the user.',
-  cooldown: 3,
+  cooldown: 15,
   execute(message, args) {
-    if (!message) return message.channel.send('The command needs a searching parameter.');
+    if (!args) return message.channel.send('The command needs a searching parameter.');
     message.channel.send(message);
-
-    if (talkedRecently.has(message.author.id)) {
-      message.channel.send("Wait  minute before getting typing this again. - ");
-    } else {
-
-      // the user can type the command ... your command code goes here :)
-
-      // Adds the user to the set so that they can't talk for a minute
-      talkedRecently.add(message.author.id);
-      setTimeout(() => {
-        // Removes the user from the set after a minute
-        talkedRecently.delete(message.author.id);
-      }, 60000);
-    }
-
   },
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const fs = require('fs');
-const Discord = require('discord.js')
-const cooldowns = new Discord.Collection();
+const Discord = require('discord.js');
+const cooldowns = new Map();
 const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -41,6 +41,32 @@ client.on('message', function (message) {
 
   if (!client.commands.has(commandName)) return;
   const command = client.commands.get(commandName);
+
+  //If cooldowns map doesn't have a command.name key then create one.
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const current_time = Date.now();
+  const time_stamps = cooldowns.get(command.name);
+  const cooldown_amount = (command.cooldown) * 1000;
+
+  //If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
+  if (time_stamps.has(message.author.id)) {
+    const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
+
+    if (current_time < expiration_time) {
+      const time_left = (expiration_time - current_time) / 1000;
+
+      return message.reply(`Please wait ${time_left.toFixed(1)} more seconds before using ${command.name}`);
+    }
+  }
+
+  //If the author's id is not in time_stamps then add them with the current time.
+  time_stamps.set(message.author.id, current_time);
+  //Delete the user's id once the cooldown is over.
+  setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
+
   try {
     command.execute(message, args);
   } catch (error) {
