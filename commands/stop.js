027036d0e19222ce9ab4ede817d39eb6dac8665b
Lucy Guzmán <analucygdl@gmail.com>
2021-09-20 17:58:58 -0500

    Test code and solve errors

--- commands/stop.js
+++ commands/stop.js
@@ -1,10 +1,12 @@
+'use strict';
 
-var ap = require('./../cfe.js');
 module.exports = {
-    name: 'stop',
-    description: 'Stop the scraping process.',
-    cooldown: 3,
-    execute(client, message, args) {
-        ap.stopSong(message.guild.id);
-    },
+  name: 'stop',
+  description: 'Stop the scraping process.',
+  cooldown: 3,
+  execute(client, message, args) {
+    message.channel.reply("Shutting down...").then(() => {
+      client.destroy();
+    })
+  },
 };
