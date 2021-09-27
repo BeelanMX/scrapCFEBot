4d0df99078c6a6e2acc9701f648b4407b0581d55
Alejandro Medina <5almero5@gmail.com>
2021-09-26 22:55:09 -0500

    Add separate fuctions

--- commands/cfe.js
+++ commands/cfe.js
@@ -5,44 +5,58 @@ const Scrapper = require('../webScraping/cfeScrapper');
 const myValidator = new Validation();
 const fs = require('fs');
 
-module.exports = {
-  name: 'cfe',
-  description: 'Get searching parameters from the user.',
-  cooldown: 3,
-  execute(message, args) {
-    if (!args || args.length == 0) {
-      return message.channel.send('The command needs a searching parameter.');
+/**
+ * transform message to string
+ * @param {JsonObject} myJSON
+ * @return {string} length of 2000
+ */
+function messageToSend(myJSON) {
+  const values = [];
+  for (let i = 0; i < myJSON.length; i++) {
+    const objectKeys = Object.keys(myJSON[i]);
+    const objectValues = Object.values(myJSON[i]);
+    for (let j = 0; j < objectKeys.length; j++) {
+      values.push(`${objectKeys[j]}: ${objectValues[j]}`);
     }
-    const route = `./assets/cfe_${args.join('').toLowerCase()}.json`;
-    args = args.join(' ');
+    values.push('\n');
+  }
+  return values.join('\n').substr(0, 2000);
+}
+/**
+ * The main function of use
+ * @param {Message} message
+ * @argument
+ */
+async function execute(message, args) {
+  if (!args || args.length == 0) {
+    return message.channel.send('The command needs a searching parameter.');
+  }
+  const route = `./assets/cfe_${args.join('').toLowerCase()}.json`;
+  args = args.join(' ');
+  try {
     const executeScrapper = myValidator.isFileLastUpdateIn(route);
     if (!executeScrapper) {
       message.reply('Is needed execute the scrapper, executing...');
       const cfeScrapper = new Scrapper(args);
-      cfeScrapper.doScraping(route);
+      cfeScrapper.printPercentage = (percentage) => {
+        message.reply(`Loading data ${percentage.toString()} %`);
+      };
+      await cfeScrapper.doScraping(route);
     } else {
       message.reply('Is not needed execute the scrapper');
     }
-    fs.readFile(`./assets/cfe_${args}.json`, (err, jsonString) => {
-      if (err) {
-        console.log('Error reading file', err);
-        return;
-      }
-      try {
-        const myJSON = JSON.parse(jsonString);
-        const values = [];
-        for (let i = 0; i < myJSON.length; i++) {
-          const objectKeys = Object.keys(myJSON[i]);
-          const objectValues = Object.values(myJSON[i]);
-          for (let j = 0; j < objectKeys.length; j++) {
-            values.push(`${objectKeys[j]}: ${objectValues[j]}`);
-          }
-          values.push('\n');
-        }
-        message.channel.send(values.join('\n').substr(0, 2000));
-      } catch (err) {
-        console.log('Error parsing JSON string:', err);
-      }
-    });
-  },
+    const jsonString = fs.readFileSync(route, 'utf8');
+    const myJSON = JSON.parse(jsonString);
+    message.channel.send(messageToSend(myJSON));
+  } catch (err) {
+    console.log('Error parsing JSON string:', err);
+    message.channel.send('There was an Error');
+  }
+}
+
+module.exports = {
+  name: 'cfe',
+  description: 'Get searching parameters from the user.',
+  cooldown: 3,
+  execute,
 };
