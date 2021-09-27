9db83f77b6c99d1c6930fc3a63ce22e4cd0e9ccc
Beatriz Martinez <trism@gmail.com>
2021-09-27 14:03:02 -0500

    Delete unused function sendTableMessage

--- utils/sendTableMessage.js
+++ utils/sendTableMessage.js
@@ -6,36 +6,6 @@
 const { MessageEmbed } = require('discord.js');
 const fs = require('fs');
 
-// eslint-disable-next-line valid-jsdoc
-/**
- * This function create the message that will send the bot
- * @param { string } route Where it will save the file created
- * @param {*} message
- */
-async function sendTableMessage(route) {
-  fs.readFile(route, (err, data) => {
-    if (err) throw err;
-    const dataFromJson = JSON.parse(data);
-    const embed = new MessageEmbed()
-        .setTitle('DATA FROM CFE')
-        .setColor(0x01a001);
-    for (let i = 0; i < dataFromJson.length; i++) {
-      const objectKeys = Object.keys(dataFromJson[i]);
-      const objectValues = Object.values(dataFromJson[i]);
-      for (let j = 0; j < objectKeys.length; j++) {
-        embed.addField(objectKeys[j], objectValues[j], true);
-      }
-      embed.addField('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
-      if (embed.length >= 5000) {
-        embed.setFooter(`The data is too long. If you want, you can see the complete data in ${route}`);
-        break;
-      }
-    }
-    // message.channel.send(embed);
-    console.log(embed);
-  });
-};
-
 /**
  *
  * @param {String} route
@@ -76,4 +46,4 @@ function jsonToEmbedMessage(route) {
   }
 }
 // eslint-disable-next-line object-curly-spacing
-module.exports = { sendTableMessage, jsonToEmbedMessage };
+module.exports = { jsonToEmbedMessage };
