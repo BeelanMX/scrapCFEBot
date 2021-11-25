796b75ede208c3773e894324fc0d98a198e1534d
Lucy Guzmán <analucygdl@gmail.com>
2021-11-25 17:47:18 -0600

    Erasing files

--- commands/clean.js
+++ commands/clean.js
@@ -4,19 +4,22 @@ module.exports = {
   cooldown: 0,
   execute(message, ARGS) {
 
-const fs = require('fs').promises
-const path = require('path')
-const AssetsFiles = './assets'
+    const fs = require('fs').promises
+    const path = require('path')
+    const FOLDER_TO_REMOVE = './assets'
 
-fs.readdir(AssetsFiles)
-  .then(files => {
-    const unlinkPromises = files.map(file => {
-      const filePath = path.join(AssetsFiles, file.endsWith('.json'))
-      return fs.unlink(filePath)
-    })
-    return Promise.all(unlinkPromises)
-  }).catch(err => {
-    message.reply(`Something wrong happened removing files of ${AssetsFiles}`)
-  })
+    fs.readdir(FOLDER_TO_REMOVE)
+      .then(files => {
+        console.log('Path')
+        const unlinkPromises = files.map(file => {
+          const filePath = path.join(FOLDER_TO_REMOVE, file)
+          return fs.unlink(filePath)
+        })
+        message.reply('Files removed succesfully')
+        return Promise.all(unlinkPromises)
+      }).catch(err => {
+        console.error('Something wrong happened removing the file', err)
+        message.reply('Something wrong happened removing the file')
+      })
 }
 }
