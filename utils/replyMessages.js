a61008d46d91a51f51de3a680fdda9d444951ae3
Beatriz Martinez <trism@gmail.com>
2021-12-02 16:16:45 -0600

    Add messages for reply

---
+++ utils/replyMessages.js
@@ -0,0 +1,33 @@
+/**
+ * This file has the possible answers for the bot or print in console.
+ */
+
+const general = {
+  errorExecution: 'Error in the execution...',
+  error: 'Error: ',
+};
+const botReplies = {
+  needsParameter: 'The command needs a searching parameter.',
+  loading: 'Loading data ',
+  dataCompleted: 'That is all the data I found.',
+  // eslint-disable-next-line quotes
+  noDataWith: "There's no data available with ",
+  needsExecuteScrapper: 'Is needed execute the scrapper, executing...',
+  noNeedsExecuteScrapper: 'Is not needed execute the scrapper.',
+};
+const consoleReplies = {
+  expectData: 'Expected data: ',
+  getData: 'Getting data...',
+  openingBrowser: 'Opening a new browser...',
+  openingTab: 'Opening a new tab...',
+  openPageCorrectly: ' has been opened successfully.',
+  fillCorrectly: 'Fields filled correctly',
+  searching: 'Searching...',
+  searchCorrect: 'Search successful',
+  noData: 'There is no data available.',
+  browserClosed: 'Browser closed successfully',
+  gotData: 'Obtained data: ',
+  savingData: 'Saving data...',
+  savedIn: 'Data saved in: ',
+  ready: 'The bot is ready for work.',
+};
