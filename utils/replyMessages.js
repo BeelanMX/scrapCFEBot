0cb8282fb05f418a32c386e4f1d1ba87f0de18b0
Beatriz Martinez <trism@gmail.com>
2021-12-03 13:28:02 -0600

    Change constants to uppercase

--- utils/replyMessages.js
+++ utils/replyMessages.js
@@ -2,32 +2,32 @@
  * This file has the possible answers for the bot or print in console.
  */
 
-const general = {
-  errorExecution: 'Error in the execution...',
-  error: 'Error: ',
+const GENERAL = {
+  ERROR_EXECUTION: 'Error in the execution...',
+  ERROR: 'Error: ',
 };
-const botReplies = {
-  needsParameter: 'The command needs a searching parameter.',
-  loading: 'Loading data ',
-  dataCompleted: 'That is all the data I found.',
+const BOT_REPLIES = {
+  NEEDS_PARAMETER: 'The command needs a searching parameter.',
+  LOADING: 'Loading data ',
+  DATA_COMPLETED: 'That is all the data I found.',
   // eslint-disable-next-line quotes
-  noDataWith: "There's no data available with ",
-  needsExecuteScrapper: 'Is needed execute the scrapper, executing...',
-  noNeedsExecuteScrapper: 'Is not needed execute the scrapper.',
+  NO_DATA_WITH: "There's no data available with ",
+  NEEDS_EXECUTE_SCRAPPER: 'Is needed execute the scrapper, executing...',
+  NO_NEEDS_EXECUTE_SCRAPPER: 'Is not needed execute the scrapper.',
 };
-const consoleReplies = {
-  expectData: 'Expected data: ',
-  getData: 'Getting data...',
-  openingBrowser: 'Opening a new browser...',
-  openingTab: 'Opening a new tab...',
-  openPageCorrectly: ' has been opened successfully.',
-  fillCorrectly: 'Fields filled correctly',
-  searching: 'Searching...',
-  searchCorrect: 'Search successful',
-  noData: 'There is no data available.',
-  browserClosed: 'Browser closed successfully',
-  gotData: 'Obtained data: ',
-  savingData: 'Saving data...',
-  savedIn: 'Data saved in: ',
-  ready: 'The bot is ready for work.',
+const CONSOLE_REPLIES = {
+  EXPECT_DATA: 'Expected data: ',
+  GET_DATA: 'Getting data...',
+  OPENING_BROWSER: 'Opening a new browser...',
+  OPENING_TAB: 'Opening a new tab...',
+  OPEN_PAGE_CORRECTLY: ' has been opened successfully.',
+  FILL_CORRECTLY: 'Fields filled correctly',
+  SEARCHING: 'Searching...',
+  SEARCH_CORRECT: 'Search successful',
+  NO_DATA: 'There is no data available.',
+  BROWSER_CLOSED: 'Browser closed successfully',
+  GET_DATA: 'Obtained data: ',
+  SAVING_DATA: 'Saving data...',
+  SAVED_IN: 'Data saved in: ',
+  READY: 'The bot is ready for work.',
 };
