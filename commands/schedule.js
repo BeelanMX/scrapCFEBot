e5548c581320f3c1b6a684871832bca383f82c17
Lucy Guzmán <analucygdl@gmail.com>
2022-01-15 21:40:39 -0600

    Get command flag

--- commands/schedule.js
+++ commands/schedule.js
@@ -4,11 +4,15 @@ module.exports = {
     name: 'schedule',
     description: 'Let the user to schedule a search.',
     cooldown: 0,
-    execute(message, args) {
-        message.reply('Command listened');
-cron.schedule('* * * * *', function() {
-    message.reply('Test message every minute');
-    console.log('running a task every minute');
-  });
+    execute(message, flag, args) {
+        message.reply('Program your search:');
+        const FLAG_ARRAY = flag[0][0].toLowerCase();
+        const VALUE = flag[0][1].toLowerCase();
+        message.reply(FLAG_ARRAY)
+        console.log(VALUE)
+        /*cron.schedule('* * * * *', function() {
+            message.reply('Test message every minute');
+            console.log('running a task every minute');
+          });*/
 },
 };
