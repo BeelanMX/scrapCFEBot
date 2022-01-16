5b30b49361fde3e118af65e30708c4febc87ae74
Lucy Guzmán <analucygdl@gmail.com>
2022-01-16 00:46:43 -0600

    Run task depending on flag

--- commands/schedule.js
+++ commands/schedule.js
@@ -5,14 +5,42 @@ module.exports = {
     description: 'Let the user to schedule a search.',
     cooldown: 0,
     execute(message, flag, args) {
-        message.reply('Program your search:');
-        const FLAG_ARRAY = flag[0][0].toLowerCase();
         const VALUE = flag[0][1].toLowerCase();
-        message.reply(FLAG_ARRAY)
-        console.log(VALUE)
-        /*cron.schedule('* * * * *', function() {
+        switch (VALUE) {
+          case 'n':
             message.reply('Test message every minute');
-            console.log('running a task every minute');
-          });*/
+            cron.schedule('* * * * *', function() {
+              message.reply('Test message every minute');
+              console.log('running a task every minute');
+            });
+            break;
+          case 'd':
+            message.reply('Test message every day');
+            cron.schedule('0 12 * * 0-6', function() {
+              //run everyday at 12pm
+              message.reply('Your search will be done everyday at 12pm');
+              console.log('running a task every day');
+            });
+            break;
+          case 'w':
+            message.reply('Your search will be done every saturday at 12pm')
+            cron.schedule('0 12 * * 6', function() {
+              //run every saturday at 12pm
+              message.reply('Test message every week');
+              console.log('running a task every week');
+            });
+            break;
+          case 'm':
+            message.reply('Your search will be done every 1st day of month at 12pm');
+            cron.schedule('0 12 1 * *', function() {
+              //run every 1st day of month at 12pm
+              message.reply('Test message every month');
+              console.log('running a task every month');
+            });
+            break;
+          default:
+            message.reply('Unrecognized flag. Use -d (every day), -w (every week) or -m (every month)');
+            break;
+        }
 },
 };
