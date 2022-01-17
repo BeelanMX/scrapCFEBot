d43d35e2b510b2bb5ec1aa0998e9d56057b583ff
Beatriz Martinez <trism@gmail.com>
2022-01-17 16:37:41 -0600

    Fix the search without flags

--- commands/schedule.js
+++ commands/schedule.js
@@ -1,52 +1,63 @@
+/* eslint-disable indent */
 const cron = require('node-cron');
 const Scrapper = require('../webScraping/cfeScrapper');
 
 module.exports = {
-    name: 'schedule',
-    description: 'Let the user to schedule a search.',
-    cooldown: 0,
-    execute(message, flag, args) {
-        const VALUE = flag[0][1].toLowerCase();
-        const SEARCH = flag[1].toLowerCase();//1 (argumento) 2 (flag -s) 3 (estado)
-        console.log(SEARCH)
-        switch (VALUE) {
-          case 'n':
-            message.reply('Test message every minute');
-            cron.schedule('* * * * *', function() {
-              message.reply('Test message every minute');
-              console.log('running a task every minute');
-              const myScrap = new Scrapper(SEARCH);
-              myScrap.doScraping();
-              message.reply('Search running')
-            });
-            break;
-          case 'd':
-            message.reply('Test message every day');
-            cron.schedule('0 12 * * 0-6', function() {
-              //run everyday at 12pm
-              message.reply('Your search will be done everyday at 12pm');
-              console.log('running a task every day');
-            });
-            break;
-          case 'w':
-            message.reply('Your search will be done every saturday at 12pm')
-            cron.schedule('0 12 * * 6', function() {
-              //run every saturday at 12pm
-              message.reply('Test message every week');
-              console.log('running a task every week');
-            });
-            break;
-          case 'm':
-            message.reply('Your search will be done every 1st day of month at 12pm');
-            cron.schedule('0 12 1 * *', function() {
-              //run every 1st day of month at 12pm
-              message.reply('Test message every month');
-              console.log('running a task every month');
-            });
-            break;
-          default:
-            message.reply('Unrecognized flag. Use -d (every day), -w (every week) or -m (every month)');
-            break;
-        }
-},
+  name: 'schedule',
+  description: 'Let the user to schedule a search.',
+  cooldown: 0,
+  execute(message, flag, args) {
+    const VALUE = flag[0][1].toLowerCase();
+    const SEARCH = flag[1].toLowerCase();
+    const ROUTE = `./assets/cfe_${SEARCH.toLowerCase()}.json`;
+    switch (VALUE) {
+      case 'n':
+        message.reply('Test message every minute');
+        // eslint-disable-next-line prettier/prettier
+        cron.schedule('* * * * *', function() {
+          message.reply('Test message every minute');
+          console.log('running a task every minute');
+          const myScrap = new Scrapper(SEARCH);
+          myScrap.doScraping(ROUTE);
+          message.reply('Search running');
+        });
+        break;
+      case 'd':
+        message.reply('Test message every day');
+        // eslint-disable-next-line prettier/prettier
+        cron.schedule('0 12 * * 0-6', function() {
+          // run everyday at 12pm
+          message.reply('Your search will be done everyday at 12pm');
+          console.log('running a task every day');
+        });
+        break;
+      case 'w':
+        message.reply('Your search will be done every saturday at 12pm');
+        // eslint-disable-next-line prettier/prettier
+        cron.schedule('0 12 * * 6', function() {
+          // run every saturday at 12pm
+          message.reply('Test message every week');
+          console.log('running a task every week');
+        });
+        break;
+      case 'm':
+        message.reply(
+          // eslint-disable-next-line prettier/prettier
+          'Your search will be done every 1st day of month at 12pm',
+        );
+        // eslint-disable-next-line prettier/prettier
+        cron.schedule('0 12 1 * *', function() {
+          // run every 1st day of month at 12pm
+          message.reply('Test message every month');
+          console.log('running a task every month');
+        });
+        break;
+      default:
+        message.reply(
+          // eslint-disable-next-line max-len
+          'Unrecognized flag. Use -d (every day), -w (every week) or -m (every month)',
+        );
+        break;
+    }
+  },
 };
