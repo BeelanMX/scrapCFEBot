2c354a060865f0dbed8378e03065c1256168245d
unknown <analucygdl@gmail.com>
2022-02-11 20:12:36 -0600

    Flags w/scrap function

--- commands/schedule.js
+++ commands/schedule.js
@@ -20,23 +20,14 @@ module.exports = {
     search = getArgs(search);
 
     switch (VALUE) {
-      case 'n':
-        // eslint-disable-next-line prettier/prettier
-        cron.schedule('* * * * *', function() {
-          message.reply('Test message every minute');
-          console.log('running a task every minute');
-          const myScrap = new Scrapper(search, FLAG);
-          myScrap.doScraping(ROUTE);
-          message.reply('Search running');
-        });
-        break;
       case 'd':
-        message.reply('Test message every day');
+        message.reply('Your search will be done every day');
         // eslint-disable-next-line prettier/prettier
         cron.schedule('0 12 * * 0-6', function() {
           // run everyday at 12pm
-          message.reply('Your search will be done everyday at 12pm');
-          console.log('running a task every day');
+          const myScrap = new Scrapper(search, FLAG);
+          myScrap.doScraping(ROUTE);
+          message.reply('Search running');
         });
         break;
       case 'w':
@@ -44,8 +35,9 @@ module.exports = {
         // eslint-disable-next-line prettier/prettier
         cron.schedule('0 12 * * 6', function() {
           // run every saturday at 12pm
-          message.reply('Test message every week');
-          console.log('running a task every week');
+          const myScrap = new Scrapper(search, FLAG);
+          myScrap.doScraping(ROUTE);
+          message.reply('Search running');
         });
         break;
       case 'm':
@@ -56,8 +48,9 @@ module.exports = {
         // eslint-disable-next-line prettier/prettier
         cron.schedule('0 12 1 * *', function() {
           // run every 1st day of month at 12pm
-          message.reply('Test message every month');
-          console.log('running a task every month');
+          const myScrap = new Scrapper(search, FLAG);
+          myScrap.doScraping(ROUTE);
+          message.reply('Search running');
         });
         break;
       default:
